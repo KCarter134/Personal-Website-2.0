3a33023fd820f585b8a08dbfa80a43b9916df91a
Kyle Carter <kcarter134@yahoo.com>
2022-11-17 17:41:49 -0500

    svg implementation and layout change on home

--- assets/js/personalsite.js
+++ assets/js/personalsite.js
@@ -1,1 +1,11 @@
-console.log("hello");
+console.log("hello mate");
+
+function showMenu() {
+    let menu = document.getElementById('dropdown-menu');
+    if (menu.style.display === 'block') {
+        menu.style.display = 'none';
+    } else {
+        menu.style.display = 'block';
+    }
+}
+
